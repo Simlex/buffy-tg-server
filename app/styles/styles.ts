65e1a508dbad989ffad4a90e176d48be4cfb64a4
Simlex <simlexafol@gmail.com>
2024-11-06 09:24:06 +0100

    feat: add styles for OrangeBgLinkButton component

---
+++ app/styles/styles.ts
@@ -0,0 +1,3 @@
+export const Styles = {
+    OrangeBgLinkButton: "bg-gradient-to-r from-orange-400 to-orange-600 text-white text-center font-bold py-3 px-4 shadow rounded-xl hover:from-orange-500 hover:to-orange-700 transition duration-300"
+}
