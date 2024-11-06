163dd4de6b3e1a9b0e8bc0eb4097ad761f4459cd
Simlex <simlexafol@gmail.com>
2024-11-06 09:21:09 +0100

    feat: add userIdCreationMap for user account creation dates and message counts

---
+++ app/constants/userMappings.ts
@@ -0,0 +1,16 @@
+export const userIdCreationMap = [
+    { userId: 1, creationDate: new Date('2013-08-14'), messageCount: 200000 },                          // Launch date: no messages
+    { userId: 150000, creationDate: new Date('2014-08-14'), messageCount: 150000 },                  // ~1 year: 5k messages
+    { userId: 550000, creationDate: new Date('2015-01-01'), messageCount: 80000 },                 // ~1.5 years: 20k messages
+    { userId: 1500000, creationDate: new Date('2015-06-01'), messageCount: 70000 },                // ~2 years: 50k messages
+    { userId: 55000000, creationDate: new Date('2016-12-01'), messageCount: 70000 },              // ~3 years: 150k messages
+    { userId: 150000000, creationDate: new Date('2017-10-01'), messageCount: 60000 },             // ~4 years: 300k messages
+    { userId: 350000000, creationDate: new Date('2018-12-01'), messageCount: 45000 },             // ~5 years: 800k messages
+    { userId: 500000000, creationDate: new Date('2019-12-01'), messageCount: 35000 },            // ~6 years: 1.5M messages
+    { userId: 1500000000, creationDate: new Date('2020-12-01'), messageCount: 30000 },           // ~7 years: 3M messages
+    { userId: 3000000000, creationDate: new Date('2021-12-01'), messageCount: 20000 },           // ~8 years: 7M messages
+    { userId: 7500000000, creationDate: new Date('2022-12-01'), messageCount: 10000 },          // ~9 years: 10M messages
+    { userId: 9500000000, creationDate: new Date('2023-12-01'), messageCount: 10000 },          // ~10 years: 40M messages
+    { userId: 25000000000, creationDate: new Date('2024-12-01'), messageCount: 5000 }         // ~11 years: 100M messages
+  ];
+  
