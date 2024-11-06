9c8d7ccdbbac7eacc8b086fb51d6b81e55ad970b
Simlex <simlexafol@gmail.com>
2024-11-06 09:21:00 +0100

    feat: add RollsPurchasesConfig for purchase pricing configuration

---
+++ app/constants/purchases.ts
@@ -0,0 +1,18 @@
+export const RollsPurchasesConfig = [
+    {
+        tonPrice: 0.5,
+        roll: 40,
+    },
+    {
+        tonPrice: 1,
+        roll: 100,
+    },
+    {
+        tonPrice: 2,
+        roll: 150,
+    },
+    {
+        tonPrice: 5,
+        roll: 350,
+    },
+]
