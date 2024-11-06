29cae637c3cab1628130edb64e102df587a42f29
Simlex <simlexafol@gmail.com>
2024-11-06 09:23:41 +0100

    feat: extend PointsUpdateRequest to include game and account metrics

--- app/models/IPoints.ts
+++ app/models/IPoints.ts
@@ -1,7 +1,10 @@
+import { Game } from "../enums/Game";
 import { Task } from "../enums/ITask";
 
 export interface PointsUpdateRequest {
     userId: string;
     points: number;
-    task?: Task
+    task?: Task;
+    game?: Game;
+    accountMetrics?: 'age' | 'messages'
 }
