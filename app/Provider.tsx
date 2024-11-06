c33a710c5e00f7ed3dbb2db0468a776ddcf25409
Simlex <simlexafol@gmail.com>
2024-11-06 09:16:56 +0100

    feat: wrap AppProvider with TonConnectUIProvider for enhanced connectivity

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
 import { AppProvider } from './context/ApplicationContext';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 type Props = {
     children?: React.ReactNode
@@ -8,9 +9,13 @@ type Props = {
 
 const GlobalProvider = ({ children }: Props) => {
     return (
-        <AppProvider>
-            {children}
-        </AppProvider>
+        <TonConnectUIProvider 
+        // manifestUrl="https://2c10-41-184-8-14.ngrok-free.app/tonconnect-manifest.json">
+        manifestUrl="https://gist.githubusercontent.com/alexcraviotto/b5d974bc120c3b46a0d047ba79cb4874/raw/2b04202d388d9547173b56c6ef27734edba18b29/tonconnect-manifest.json">
+            <AppProvider>
+                {children}
+            </AppProvider>
+        </TonConnectUIProvider>
     );
 };
 
