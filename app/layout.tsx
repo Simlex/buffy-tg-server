28ece1c0022eaa2eb099ee8110a4ee0d455e56c4
Simlex <simlexafol@gmail.com>
2024-11-06 09:16:39 +0100

    feat: add manifest link and import Script component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Layout, { WrappedLayout } from './components/Layout';
 import GlobalProvider from './Provider';
 import Image from 'next/image';
 import images from '@/public/images';
+import Script from 'next/script';
 
 const dmSans = Outfit({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default async function RootLayout({
         <html lang="en" data-theme={"light"} className='bg-white'>
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1, user-scalable=no" />
             <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+            <link rel="manifest" href="/tonconnect-manifest.json" />
 
             <GlobalProvider>
                 <body className={`${dmSans.className} p-6 relative bg-gradient-to-b from-slate-800 to-black to-slate-0`}>
