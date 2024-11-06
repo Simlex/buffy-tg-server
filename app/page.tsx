b065d6b64995c008f7b52255bf503deebb6703f0
Simlex <simlexafol@gmail.com>
2024-11-06 09:16:49 +0100

    feat: add client-side imports and commented out Telegram WebApp biometric access logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+"use client"
+import Image from "next/image";
+import Button from "./components/ui/button";
 import Homepage from "./homepage/Homepage";
+import images from "@/public/images";
+import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+    // useEffect(() => {
+    //     // Check if Telegram WebApp is available
+    //     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+    //         const tg = window.Telegram.WebApp;
+
+    //         if (tg.BiometricManager.isInited) {
+    //             tg.HapticFeedback.impactOccurred("light");
+    //             !tg.BiometricManager.isAccessRequested &&
+    //                 tg.BiometricManager.requestAccess({
+    //                     reason: "Biometric Access",
+    //                 }, (granted) => {
+    //                     if (granted) {
+    //                         tg.BiometricManager.authenticate({
+    //                             reason: "Biometric Access",
+    //                         }, (success, token) => {
+    //                             if (success) {
+    //                                 console.log("🚀 ~ Home ~ token:", token)
+    //                             }
+    //                         })
+    //                     }
+    //                 });
+    //         }
+    //     }
+    // }, []);
     return (
         <Homepage />
     );
