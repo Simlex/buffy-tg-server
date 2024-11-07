a3f61cbea83b889665675eb1b06f558aae145546
Simlex <simlexafol@gmail.com>
2024-11-07 15:25:35 +0100

    refactor: add ButtonLoader component for enhanced loading UI

--- app/components/Loader/ComponentLoader.tsx
+++ app/components/Loader/ComponentLoader.tsx
@@ -4,6 +4,14 @@ interface ComponentLoaderProps {
     className?: string;
 }
 
+type ComponentLoaderV2Props = {
+
+    /**
+     * The optional classnames to be added
+     */
+    className?: string
+}
+
 const ComponentLoader: FunctionComponent<ComponentLoaderProps> = ({ className: classNamw }): ReactElement => {
 
     return (
@@ -12,3 +20,13 @@ const ComponentLoader: FunctionComponent<ComponentLoaderProps> = ({ className: c
 }
 
 export default ComponentLoader;
+
+export const ButtonLoader: FunctionComponent<ComponentLoaderV2Props> = (
+    { className }): ReactElement => {
+
+    return (
+        <div className='absolute w-full h-full top-0 left-0 bg-white grid place-items-center pointer-events-none'>
+            <div className={`w-6 h-6 border-4 border-orange-500 border-t-transparent border-solid rounded-full animate-spin ${className}`} />
+        </div>
+    );
+}
