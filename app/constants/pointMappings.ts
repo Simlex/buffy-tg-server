b67e802070725b87d2d24762b1257de417f11aae
Simlex <simlexafol@gmail.com>
2024-11-07 15:25:46 +0100

    refactor: add pointMappings constant for dice roll point allocation

---
+++ app/constants/pointMappings.ts
@@ -0,0 +1,36 @@
+export const pointsMappings = [
+    {
+        diceRoll: 1,
+        points: 2000,
+        ton: null,
+        nft: null
+    },
+    {
+        diceRoll: 2,
+        points: 15000,
+        ton: null,
+        nft: null
+    },
+    {
+        diceRoll: 3,
+        points: 50000,
+        ton: null,
+        nft: null
+    },
+    {
+        diceRoll: 4,
+        points: null,
+        ton: 2
+    },
+    {
+        diceRoll: 5,
+        points: null,
+        ton: 7.5
+    },
+    {
+        diceRoll: 6,
+        points: null,
+        ton: null,
+        nft: 1
+    }
+]
