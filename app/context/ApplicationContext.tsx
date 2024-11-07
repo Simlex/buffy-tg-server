cd21601c644f2ab45096816481bc1acd27aa4e62
Simlex <simlexafol@gmail.com>
2024-11-07 15:25:50 +0100

    refactor: add taps state and didInitialLoad ref to ApplicationContext for improved user interaction tracking

--- app/context/ApplicationContext.tsx
+++ app/context/ApplicationContext.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactNode, createContext, useState } from "react";
+import { FunctionComponent, MutableRefObject, ReactNode, createContext, useRef, useState } from "react";
 import { UserProfileInformation } from "../models/IUser";
 import { StorageKeys } from "../constants/storageKeys";
 import { fetchUserFromDb } from "../api/services/fetchUserFromDb";
@@ -22,6 +22,9 @@ export interface ApplicationContextData {
     updateTimesClickedPerSession: (times: number) => void;
     selectedGame: Game;
     updateSelectedGame: (game: Game) => void;
+    taps: number;
+    setTaps: (taps: number) => void;
+    didInitialLoad: MutableRefObject<boolean>
 };
 
 // Create a context with the specified data type
@@ -40,6 +43,8 @@ const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
     const [nextUpdateTimestamp, setNextUpdateTimestamp] = useState<number>(0);
     const [timeLeft, setTimeLeft] = useState<string>('');
     const [timesClickedPerSession, setTimesClickedPerSession] = useState<number>();
+    const [taps, setTaps] = useState<number>(0);
+    const didInitialLoad = useRef(false); // New ref to track initial load
 
     // Define state for displaying login prompt
     const [showUserLoginPrompt, setShowUserLoginPrompt] = useState(false);
@@ -66,11 +71,12 @@ const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
         if (_userInfo !== null || _userInfo !== undefined) {
 
             const user = await fetchUserFromDb(_userInfo.userId);
-            
-            console.log("🚀 ~ handleFetchUserInformation ~ user:", user)
 
             // Set the user information
             setUserProfileInformation(user);
+
+            // Set tap points
+            setTaps(user.tapPoints || 0);
         };
     };
 
@@ -91,6 +97,9 @@ const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
         updateTimesClickedPerSession: (times: number) => setTimesClickedPerSession(times),
         selectedGame,
         updateSelectedGame: (game: Game) => setSelectedGame(game),
+        taps,
+        setTaps,
+        didInitialLoad
     };
 
     return (
