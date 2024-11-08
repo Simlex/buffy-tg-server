675b77b11c937940e901b6d146e9064d98bed2f0
Simlex <simlexafol@gmail.com>
2024-11-08 10:05:58 +0100

    feat: implement deposit functionality and update user rolls points in WalletPage component

--- app/wallet/WalletPage.tsx
+++ app/wallet/WalletPage.tsx
@@ -1,13 +1,19 @@
 "use client"
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Icons } from '../components/ui/icons'
 import { RollsPurchasesConfig } from '../constants/purchases'
 import ConnectWalletModal from '../components/modal/ConnectWalletModal'
 import { SendTransactionRequest, TonConnectUIContext, useTonAddress, useTonConnectModal } from '@tonconnect/ui-react'
 import { Address, beginCell, toNano } from '@ton/ton';
 import { motion } from 'framer-motion'
+import { useUpdateUserRollsPoints } from '../api/apiClient'
+import { ApplicationContext, ApplicationContextData } from '../context/ApplicationContext'
+import { PointsUpdateRequest } from '../models/IPoints'
+import { ButtonLoader } from '../components/Loader/ComponentLoader'
 
 export default function WalletPage() {
+    const updateUserRollsPoints = useUpdateUserRollsPoints();
+    const { userProfileInformation, updateUserProfileInformation } = useContext(ApplicationContext) as ApplicationContextData;
     const tonConnectUI = useContext(TonConnectUIContext);
 
     const userFriendlyAddress = useTonAddress();
@@ -19,7 +25,8 @@ export default function WalletPage() {
     const [isModalVisible, setIsModalVisible] = React.useState(false);
 
     const destination = userFriendlyAddress ? Address.parse(userFriendlyAddress).toRawString() : '';
-    const depositAmount = 0.5; // 0.5 TON
+    const [depositAmount, setDepositAmount] = React.useState<number>();
+    const [isDepositing, setIsDepositing] = React.useState(false);
 
     const body = beginCell()
         .storeUint(0, 32) // Write 32 zero bits to indicate a text comment will follow
@@ -30,7 +37,7 @@ export default function WalletPage() {
         messages: [
             {
                 address: destination,
-                amount: toNano(depositAmount).toString(),
+                amount: toNano(depositAmount || 0).toString(),
                 payload: body.toBoc().toString('base64'), // Optional: Additional data
             },
         ],
@@ -39,26 +46,56 @@ export default function WalletPage() {
     };
 
     const handleBuyRolls = () => {
+        if (!depositAmount) {
+            console.log("🚀 ~ handleBuyRolls ~ depositAmount:", depositAmount)
+            return;
+        };
+
         if (!tonConnectUI?.connected) {
             open();
             // setIsModalVisible(true);
             return;
         }
 
+        setIsDepositing(true);
+
         if (tonConnectUI) {
             tonConnectUI
                 .sendTransaction(paymentRequest)
-                .then((transactionResult) => {
+                .then(async (transactionResult) => {
                     console.log('Transaction successful:', transactionResult);
+
+                    const data: PointsUpdateRequest = {
+                        userId: userProfileInformation?.userId as string,
+                        points: RollsPurchasesConfig.find(purchase => purchase.tonPrice === depositAmount)?.roll || 0
+                    }
+
+                    await updateUserRollsPoints(data)
+                        .then((response) => {
+                            updateUserProfileInformation(response.data);
+                        })
+                        .catch((error) => {
+                            console.error('Transaction failed:', error);
+                        })
+                        .finally(() => { })
                 })
                 .catch((error) => {
                     console.error('Transaction failed:', error);
+                })
+                .finally(() => {
+                    setIsDepositing(false);
+                    setDepositAmount(undefined);
                 });
         } else {
             console.log('Wallet is not connected');
             alert(`Wallet is not connected `);
+            setIsDepositing(false);
         }
-    }
+    };
+
+    useEffect(() => {
+        handleBuyRolls();
+    }, [depositAmount]);
 
     return (
         <>
@@ -67,10 +104,24 @@ export default function WalletPage() {
                 setVisibility={setIsModalVisible}
             />
             <main className="flex min-h-screen flex-col items-center py-14 pb-32">
-                <span className='w-24 h-24 rounded-full bg-white/10 grid place-items-center border-8 border-orange-500/5 mb-3'>
-                    <Icons.Cart className='w-12 h-12' fill='#fff' />
+                <span className='w-28 h-28 rounded-full bg-white/0 grid place-items-center border-[10px] border-[#24A1DE]/5 mb-3'>
+                    {/* <Icons.Cart className='w-12 h-12' fill='#fff' /> */}
+                    <motion.span
+                        animate={{
+                            scale: [1, 0.9, 1],
+                            // transformStyle: "preserve-3d",
+                            // transform: ["translateY(0px)", "translateY(-10px)", "translateY(0px)"], 
+                            transitionBehavior: "ease-in-out",
+                            transition: {
+                                duration: 2.5,
+                                repeat: Infinity
+                            }
+                        }}
+                        className='text-[64px]'>
+                        💎
+                    </motion.span>
                 </span>
-                <div className='mb-14 text-center'>
+                <div className='mb-10 text-center'>
                     <h2 className="text-white font-medium text-3xl">Rolls Purchase</h2>
                     <p className='text-white/70'>You can purchase rolls for dice roll game here</p>
                 </div>
@@ -90,7 +141,28 @@ export default function WalletPage() {
                     </button>
                 </div> */}
 
+                <div className='flex flex-col w-full mb-4'>
+                    <span className='text-white mb-2'>Premium</span>
+                    <div className=' bg-gradient-to-br from-[#24A1DE] to-[#086b9c] p-4 rounded-3xl flex items-center justify-between mb-4'>
+                        <div>
+                            <h3 className='text-white font-bold'>4 daily rolls + other perks</h3>
+                            <p className='text-white/60'>2.5 TON</p>
+                        </div>
+                        <motion.button
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => {
+                                setDepositAmount(2.5);
+                            }}
+                            disabled={isDepositing}
+                            className='bg-orange-500 text-white font-bold py-2 px-4 rounded-2xl relative overflow-hidden disabled:pointer-events-none'>
+                            Buy
+                            {isDepositing && depositAmount == 2.5 && <ButtonLoader />}
+                        </motion.button>
+                    </div>
+                </div>
+
                 <div className='flex flex-col w-full'>
+                    <span className='text-white mb-2'>Others</span>
                     {
                         RollsPurchasesConfig.map((purchase, index) => (
                             <div key={index} className='bg-white/20 p-4 rounded-3xl flex items-center justify-between mb-4'>
@@ -100,8 +172,13 @@ export default function WalletPage() {
                                 </div>
                                 <motion.button
                                     whileTap={{ scale: 0.9 }}
-                                    onClick={() => handleBuyRolls()} className='bg-orange-500 text-white font-bold py-2 px-4 rounded-2xl'>
+                                    onClick={() => {
+                                        setDepositAmount(purchase.tonPrice);
+                                    }}
+                                    disabled={isDepositing}
+                                    className='bg-orange-500 text-white font-bold py-2 px-4 rounded-2xl relative overflow-hidden disabled:pointer-events-none'>
                                     Buy
+                                    {isDepositing && depositAmount == purchase.tonPrice && <ButtonLoader />}
                                 </motion.button>
                             </div>
                         ))
