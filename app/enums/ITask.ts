5fbb385ab55f0aed0da78371022bb17c1add0636
Simlex <simlexafol@gmail.com>
2024-11-10 15:06:07 +0100

    feat: extend Task enum with WALLET_CONNECT and TON_TRANSACTION, and add Others to TaskType

--- app/enums/ITask.ts
+++ app/enums/ITask.ts
@@ -1,9 +1,12 @@
 export enum Task {
     TELEGRAM = "telegram",
-    TWITTER = "twitter"
+    TWITTER = "twitter",
+    WALLET_CONNECT = "walletConnect",
+    TON_TRANSACTION = "tonTransaction",
 }
 
 export enum TaskType {
     Social,
-    Referral
+    Referral,
+    Others
 }
