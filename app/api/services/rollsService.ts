5fe79927307365dce67e37e1f1c0752c15616ac2
Simlex <simlexafol@gmail.com>
2024-11-13 14:29:01 +0100

    feat: enhance updateUserRollsPoints function with logging and improved error handling

--- app/api/services/rollsService.ts
+++ app/api/services/rollsService.ts
@@ -6,49 +6,58 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export async function updateUserRollsPoints(req: NextRequest) {
-    // Get the body of the request
-    const request = (await req.json()) as PointsUpdateRequest;
-  
-    // Check if all required fields are provided
-    if (!request.userId) {
-      return {
-        error: ApplicationError.MissingRequiredParameters.Text,
-        statusCode: StatusCodes.BadRequest,
-      };
-    }
-  
-    // Check if user exists
-    const user = await prisma.users.findUnique({
-      where: {
-        userId: request.userId,
-      },
-    });
-  
-    // If user does not exists, return error
-    if (!user) {
-      return {
-        error: ApplicationError.UserWithIdNotFound.Text,
-        errorCode: ApplicationError.UserWithIdNotFound.Code,
-        statusCode: StatusCodes.NotFound,
-      };
-    }
+  // Get the body of the request
+  const request = (await req.json()) as PointsUpdateRequest;
+  console.log("🚀 ~ updateUserRollsPoints ~ request:", request);
 
-    // get the dice rolls the user gets from this purchase
-    const diceRolls = RollsPurchasesConfig.find(purchase => purchase.tonPrice === request.ton)?.roll || 0;
-  
-    // Update the user's dice roll points
-    const updatedUser = await prisma.users.update({
-      where: {
-        userId: request.userId,
-      },
-      data: {
-        isSubscribedToPremium: request.forPremiumSubscription,
-        availableDiceRolls: {
-            increment: diceRolls
-        },
-      },
-    });
-  
-    // Return the response
-    return { ...updatedUser };
+  // Check if all required fields are provided
+  if (!request.userId) {
+    return {
+      error: ApplicationError.MissingRequiredParameters.Text,
+      statusCode: StatusCodes.BadRequest,
+    };
   }
+
+  // Check if user exists
+  const user = await prisma.users.findUnique({
+    where: {
+      userId: request.userId,
+    },
+  });
+
+  // If user does not exists, return error
+  if (!user) {
+    return {
+      error: ApplicationError.UserWithIdNotFound.Text,
+      errorCode: ApplicationError.UserWithIdNotFound.Code,
+      statusCode: StatusCodes.NotFound,
+    };
+  }
+
+  // get the dice rolls the user gets from this purchase
+  const diceRolls =
+    RollsPurchasesConfig.find((purchase) => purchase.tonPrice === request.ton)
+      ?.roll || 0;
+
+  // Update the user's dice roll points
+  const updatedUser = await prisma.users.update({
+    where: {
+      userId: request.userId,
+    },
+    data: {
+      isSubscribedToPremium: request.forPremiumSubscription,
+      availableDiceRolls: {
+        increment: diceRolls,
+      },
+      tonEarned: {
+        increment: request.ton ?? 0,
+      },
+      nftEarned: {
+        increment: request.nft ?? 0,
+      },
+    },
+  });
+
+  // Return the response
+  return { ...updatedUser };
+}
