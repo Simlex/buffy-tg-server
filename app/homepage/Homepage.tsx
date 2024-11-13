61ccd2de2a0e837bbc72b5a1c4bf2c4d23077b2b
Simlex <simlexafol@gmail.com>
2024-11-13 14:35:24 +0100

    feat: enhance Homepage component with additional user points display and icons

--- app/homepage/Homepage.tsx
+++ app/homepage/Homepage.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import images from "@/public/images";
 import Link from "next/link";
 import { ApplicationContext, ApplicationContextData } from "../context/ApplicationContext";
+import { Icons } from "../components/ui/icons";
 
 
 const Homepage: FunctionComponent = (): ReactElement => {
@@ -23,7 +24,33 @@ const Homepage: FunctionComponent = (): ReactElement => {
                     <Image src={images.coin} className="w-full h-full object-contain" alt="Buffy" />
                 </span>
                 <p className="text-sm text-white/50">Total Points</p>
-                <h1 className="text-[40px] text-white font-extrabold">{(userProfileInformation?.totalPoints ?? 0).toLocaleString()}</h1>
+                <h1 className="text-[40px] text-white font-extrabold mb-2">{(userProfileInformation?.totalPoints ?? 0).toLocaleString()}</h1>
+                
+                {/* <div className="flex flex-row gap-2 items-center mb-2">
+                    <span className="w-7 h-7 relative grid place-items-center">
+                        <CustomImage src={images.coin} alt="Coin" />
+                    </span>
+                    <motion.h1
+                        key={userProfileInformation?.diceRollsPoints}
+                        initial={{ scale: 0.5, opacity: 0, filter: "blur(10px)" }}
+                        animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
+                        exit={{ scale: 0, opacity: 0 }}
+                        className="text-[40px] text-white font-extrabold">
+                        {(userProfileInformation?.diceRollsPoints ?? 0).toLocaleString()}
+                    </motion.h1>
+                </div> */}
+                <div className="flex flex-row gap-2 items-center text-white/80 mb-4">
+                    <div className="flex flex-row items-center gap-2 p-2 px-3 pr-2 bg-white/10 rounded-3xl">
+                        <p>{userProfileInformation?.tonEarned} TON</p>
+                        <span><Icons.Ton fill="#fff" className="w-5 h-5" /></span>
+                    </div>
+                    <div className="flex flex-row items-center gap-2 p-2 px-3 pr-2 bg-white/10 rounded-3xl">
+                        <p>{userProfileInformation?.nftEarned} NFT</p>
+                        <span>
+                            <Image src={images.nft_coin} alt="Nft coin" className="w-5 h-5" />
+                        </span>
+                    </div>
+                </div>
             </div>
             <div className="flex flex-col gap-2 w-full mt-auto mb-12">
                 <p className="text-white/50">Get more points</p>
