8cb48535390af643f22dc304ed8d776cceb08aae
Simlex <simlexafol@gmail.com>
2024-11-15 09:57:36 +0100

    feat: add POST endpoint for updating user daily roll streak in rolls API

---
+++ app/api/rolls/streak/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validateRequestMethod } from "../../services/reusable-services/requestMethodValidator";
+import { customNextResponseError } from "../../utils/customNextResponseError";
+import { StatusCodes } from "@/app/models/IStatusCodes";
+import { ApplicationError } from "@/app/constants/applicationError";
+import { updateUserDailyRollStreak } from "../../services/rollsService";
+
+export async function POST(req: NextRequest) {
+  // Call the request validation method
+  await validateRequestMethod(req, "POST");
+
+  try {
+    // Call the update user daily roll streak function
+    const operation = await updateUserDailyRollStreak(req);
+
+    // If the operation fails, return an error
+    if (operation.error) {
+      return customNextResponseError(operation);
+    }
+
+    // Return the response
+    return NextResponse.json(operation, { status: StatusCodes.Created });
+  } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
+    // Return an error if the operation fails
+    return NextResponse.json(
+      { error: ApplicationError.FailedToUpdateUserPoints.Text },
+      { status: StatusCodes.InternalServerError }
+    );
+  }
+}
