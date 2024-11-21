85e60e6a40ab68cbf131961b7cfa5b5f0efd2438
Simlex <simlexafol@gmail.com>
2024-11-21 10:17:06 +0100

    refactor: update TonConnect manifest URL to use a relative path

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -12,7 +12,8 @@ const GlobalProvider = ({ children }: Props) => {
         <TonConnectUIProvider
             // manifestUrl="https://2c10-41-184-8-14.ngrok-free.app/tonconnect-manifest.json">
             // manifestUrl="https://gist.githubusercontent.com/alexcraviotto/b5d974bc120c3b46a0d047ba79cb4874/raw/2b04202d388d9547173b56c6ef27734edba18b29/tonconnect-manifest.json">
-            manifestUrl="https://buffy-tg-server.vercel.app/tonconnect-manifest.json">
+            // manifestUrl="https://buffy-tg-server.vercel.app/tonconnect-manifest.json">
+            manifestUrl="/tonconnect-manifest.json">
             <AppProvider>
                 {children}
             </AppProvider>
