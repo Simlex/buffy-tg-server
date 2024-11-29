9df1771f0b46d2f0f5d0df487966af95e3eaf8a6
Simlex <simlexafol@gmail.com>
2024-11-29 09:24:32 +0100

    feat: implement GET endpoint to fetch bot users with error handling

---
+++ app/api/admin/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validateRequestMethod } from "../services/reusable-services/requestMethodValidator";
+import { customNextResponseError } from "../utils/customNextResponseError";
+import { ApplicationError } from "@/app/constants/applicationError";
+import { StatusCodes } from "@/app/models/IStatusCodes";
+import { fetchBotUsers } from "../services/adminService";
+
+export async function GET(req: NextRequest) {
+    // Call the request validation method
+    await validateRequestMethod(req, "GET");
+  
+    try {
+      // Call the function to fetch bot users
+      const operation = await fetchBotUsers(req);
+  
+      // If the operation fails, return an error
+      if (operation.error) {
+        return customNextResponseError(operation);
+      }
+  
+      // Return the response
+      return NextResponse.json(operation.data, { status: StatusCodes.Success });
+    } catch {
+      // Return an error if the operation fails
+      return NextResponse.json(
+        { error: ApplicationError.FailedToFetchUsers.Text },
+        { status: StatusCodes.InternalServerError }
+      );
+    }
+  }
