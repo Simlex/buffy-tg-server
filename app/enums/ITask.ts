3ef54b1e18777943d9735fe8aa012068a72bd4ba
Simlex <simlexafol@gmail.com>
2024-12-06 09:06:32 +0100

    feat: add DICE_SPIN_15 and DICE_SPIN_75 tasks to ITask enum

--- app/enums/ITask.ts
+++ app/enums/ITask.ts
@@ -4,6 +4,8 @@ export enum Task {
     WALLET_CONNECT = "walletConnect",
     TON_TRANSACTION = "tonTransaction",
     WEBSITE_VIEW = "website_view",
+    DICE_SPIN_15 = "dice_spin_15",
+    DICE_SPIN_75 = "dice_spin_75",
 }
 
 export enum TaskType {
