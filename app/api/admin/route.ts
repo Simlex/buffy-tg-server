3851c22c62b86b5fe34fb95d9954a437b636b06f
Simlex <simlexafol@gmail.com>
2024-12-09 21:46:25 +0100

    fix: improve error handling and response structure in bot users fetch route

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -6,25 +6,25 @@ import { StatusCodes } from "@/app/models/IStatusCodes";
 import { fetchBotUsers } from "../services/adminService";
 
 export async function GET(req: NextRequest) {
-    // Call the request validation method
-    await validateRequestMethod(req, "GET");
-  
-    try {
-      // Call the function to fetch bot users
-      const operation = await fetchBotUsers(req);
-  
-      // If the operation fails, return an error
-      if (operation.error) {
-        return customNextResponseError(operation);
-      }
-  
-      // Return the response
-      return NextResponse.json(operation.data, { status: StatusCodes.Success });
-    } catch {
-      // Return an error if the operation fails
-      return NextResponse.json(
-        { error: ApplicationError.FailedToFetchUsers.Text },
-        { status: StatusCodes.InternalServerError }
-      );
+  // Call the request validation method
+  await validateRequestMethod(req, "GET");
+
+  try {
+    // Call the function to fetch bot users
+    const operation = await fetchBotUsers(req);
+
+    // If the operation fails, return an error
+    if (operation.error) {
+      return customNextResponseError(operation);
     }
+
+    // Return the response
+    return NextResponse.json(operation.data, { status: StatusCodes.Success });
+  } catch {
+    // Return an error if the operation fails
+    return NextResponse.json(
+      { error: ApplicationError.FailedToFetchUsers.Text },
+      { status: StatusCodes.InternalServerError }
+    );
   }
+}
