cec19434801cbaed6f15d1d37c316acf3c17bdaf
Simlex <simlexafol@gmail.com>
2024-12-09 21:46:36 +0100

    feat: add function to restrict bot users in admin API service

--- app/api/services/adminService.ts
+++ app/api/services/adminService.ts
@@ -23,6 +23,7 @@ export async function fetchBotUsers(req: NextRequest) {
   const users = await prisma.users.findMany({
     select: {
       userId: true,
+      username: true,
       totalPoints: true,
       referralCount: true,
       tonEarned: true,
@@ -36,4 +37,43 @@ export async function fetchBotUsers(req: NextRequest) {
 
   // Return all users
   return { data: users };
+}
+
+export async function restrictBotUser(req: NextRequest) {
+  // Get the search params from the request url
+  const searchParams = new URLSearchParams(req.url.split("?")[1]);
+
+  // Get the passKey from the search params
+  const passKey = searchParams.get("passkey");
+
+  // If the passkey was not provided, throw an error
+  if (!passKey || passKey !== process.env.PASSKEY) {
+    return {
+      error: ApplicationError.MissingRequiredParameters.Text,
+      errorCode: ApplicationError.MissingRequiredParameters.Code,
+      statusCode: StatusCodes.NotFound,
+    };
+  }
+
+  // Get the user id from the search params
+  const userId = searchParams.get("userId");
+
+  // If the user id was not provided, throw an error
+  if (!userId) {
+    return {
+      error: ApplicationError.MissingRequiredParameters.Text,
+      errorCode: ApplicationError.MissingRequiredParameters.Code,
+      statusCode: StatusCodes.NotFound,
+    };
+  }
+
+  // Update the user to be deleted
+  await prisma.users.delete({
+    where: {
+      userId,
+    },
+  });
+
+  // Return the success message
+    return { data: "User restricted successfully" };
 }
