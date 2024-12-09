9a9026e7cbac5bcfb1cef636a8f4c2b20941fcce
Simlex <simlexafol@gmail.com>
2024-12-09 16:45:39 +0100

    fix: update trivia start date to December 9, 2024

--- app/constants/triviaConfig.ts
+++ app/constants/triviaConfig.ts
@@ -1,5 +1,5 @@
 export const TriviaConfig = {
-  startDate: new Date("2024-11-25T00:00:00Z"),
+  startDate: new Date("2024-12-09T00:00:00Z"),
   //   endDate: new Date("2021-10-31T23:59:59Z"),
   dailyTriviaPoints: 1000,
   // questionsAndAnswers: [
