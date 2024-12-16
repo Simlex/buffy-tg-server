7f91a19119d8f40ea200c5fa9229e2bb1d999816
Simlex <simlexafol@gmail.com>
2024-12-16 09:37:25 +0100

    feat: add JOIN_ERAX task and PARTNERS task type to enhance task enumeration

--- app/enums/ITask.ts
+++ app/enums/ITask.ts
@@ -3,6 +3,7 @@ export enum Task {
     TWITTER = "twitter",
     WALLET_CONNECT = "walletConnect",
     TON_TRANSACTION = "tonTransaction",
+    JOIN_ERAX = "joinErax",
     WEBSITE_VIEW = "website_view",
     DICE_SPIN_15 = "dice_spin_15",
     DICE_SPIN_75 = "dice_spin_75",
@@ -11,5 +12,6 @@ export enum Task {
 export enum TaskType {
     Social,
     Referral,
+    Partners,
     Others
 }
