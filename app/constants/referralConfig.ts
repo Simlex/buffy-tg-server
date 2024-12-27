bd7880414ef74d7602f8969f8ead6c29fc42aeae
Simlex <simlexafol@gmail.com>
2024-12-27 11:33:49 +0100

    feat: adjust contest start date in referral configuration for timezone accuracy

--- app/constants/referralConfig.ts
+++ app/constants/referralConfig.ts
@@ -1,4 +1,4 @@
 export const ReferralConfig = {
-  contestStartDate: new Date("2024-12-27T11:30:00Z"), // remove 1 hour to the hour due to timezone difference
+  contestStartDate: new Date("2024-12-27T10:30:00Z"), // remove 1 hour to the hour due to timezone difference
   contestEndDate: new Date("2025-01-26T15:00:00Z"), // remove 1 hour to the hour due to timezone difference
 };
