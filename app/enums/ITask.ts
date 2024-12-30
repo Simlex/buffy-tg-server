94182452c56141fc434e76c73635d23bdd2e95f1
Simlex <simlexafol@gmail.com>
2024-12-30 16:35:09 +0100

    fix: add PLAY_KOLO_BLOCK task to ITask enum

--- app/enums/ITask.ts
+++ app/enums/ITask.ts
@@ -5,6 +5,7 @@ export enum Task {
     TON_TRANSACTION = "tonTransaction",
     JOIN_ERAX = "joinErax",
     ZAE_CRYPTO_SUBSCRIPTION = "zaeCryptoSubscription",
+    PLAY_KOLO_BLOCK = "playKoloBlock",
     WEBSITE_VIEW = "website_view",
     DICE_SPIN_15 = "dice_spin_15",
     DICE_SPIN_75 = "dice_spin_75",
