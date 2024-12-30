6884e23c50f6c607300dacd6b7c38a95980eed04
Simlex <simlexafol@gmail.com>
2024-12-30 16:56:19 +0100

    fix: comment out startScheduler import in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Outfit } from 'next/font/google'
 import './globals.scss';
 import GlobalProvider from './Provider';
 import { WrappedLayout } from './components/Layout';
-import { startScheduler } from './scripts/scheduler';
+// import { startScheduler } from './scripts/scheduler';
 
 const dmSans = Outfit({ subsets: ['latin'] })
 
